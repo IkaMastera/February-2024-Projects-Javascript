8462598a23d8c127f42febd5e99466a256499ac9
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-02-09 14:43:34 +0400

    add: javascript onmouse function

--- Background Image Changer/changer.js
+++ Background Image Changer/changer.js
@@ -1,2 +1,12 @@
+var imgBox = document.querySelector(".img-box");
+var imgWrap = document.querySelector(".img-wrap");
+var originalImg = document.getElementById("originalImg");
 
+originalImg.style.width = imgBox.offsetWidth + "px";
 
+var leftSpace = imgBox.offsetLeft;
+
+imgBox.onmousemove = function(e){
+    var boxWidth = e.pageX - leftSpace + "px";
+    imgWrap.style.width = boxWidth;
+}
