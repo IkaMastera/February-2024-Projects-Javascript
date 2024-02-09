6bc653b355bcf1eb27ecf2060949e5b2bc919c04
Irakli <112602982+IkaMastera@users.noreply.github.com>
2024-02-09 13:50:22 +0400

    add: all variables and rocket animation

---
+++ Coming Soon Page/soon.js
@@ -0,0 +1,9 @@
+var countDownDate = new Date("February 9, 2024 00:00:00").getTime();
+var x = setInterval(function(){
+   var now = new Date().getTime();
+   var distance = countDownDate - now;
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var hours = Math.floor((distance / (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance / (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance / (1000 * 60)) / 1000);
+},1000);
